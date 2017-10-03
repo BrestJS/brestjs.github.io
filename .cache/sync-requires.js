ffe075c18da502b34105fa77b1ccd244d303481e
Glenn Guegan <glenn.guegan@gmail.com>
2017-10-03 16:51:57 +0200

    publish invit slack

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -3,12 +3,10 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/glenn/brestJS/brestjs/.cache/dev-404-page.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/glenn/brestJS/brestjs/src/pages/index.js"))
 }
 
 exports.json = {
-  "dev-404-page.json": require("/Users/glenn/brestJS/brestjs/.cache/json/dev-404-page.json"),
   "index.json": require("/Users/glenn/brestJS/brestjs/.cache/json/index.json")
 }
 
